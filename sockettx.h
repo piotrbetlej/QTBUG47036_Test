3c880b0e06e92ffa7df77f6c127b2dd2bdf0c924
Piotr Betlej <piotr.betlej@googlemail.com>
2015-07-06 22:13:30 +0200

    Simple TCP server

---
+++ sockettx.h
@@ -0,0 +1,24 @@
+#ifndef SOCKETTX_H
+#define SOCKETTX_H
+
+#include <QObject>
+#include <QTcpSocket>
+#include <QTcpServer>
+#include <QDebug>
+
+class MyTcpServer : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MyTcpServer(QObject *parent = 0);
+
+signals:
+
+public slots:
+    void newConnection();
+
+private:
+    QTcpServer *server;
+};
+
+#endif // MYTCPSERVER_H
